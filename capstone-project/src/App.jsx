8ade17b4279f6a3dded23a23265c9e134deb406b
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-02-16 07:35:13 -0600

    imported Navbar component; deleted App.css file that is no longer used

--- capstone-project/src/App.jsx
+++ capstone-project/src/App.jsx
@@ -1,19 +1,23 @@
-import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { MakeAPrayerRequest } from './pages/MakeAPrayerRequest';
 import { Signin } from './pages/Signin';
 import { Signup } from './pages/Signup';
+import { Navbar } from './components/Navbar';
 
 function App() {
     return(
+        <>
+        <Navbar />
     <Routes>
         <Route index element={<Home />} />
         <Route path='/request' element={<MakeAPrayerRequest />} />
         <Route path='/signin' element={<Signin />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='*' element={<h1>404: Page Not Found</h1>} />
-    </Routes>)
+    </Routes>
+    </>
+    )
 }
 
 export default App;
