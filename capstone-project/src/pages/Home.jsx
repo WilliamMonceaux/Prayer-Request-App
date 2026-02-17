fe827db75bd0cd69b1e007b6301f4435947c0935
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-02-17 08:06:39 -0600

    added prop children text for header and paragraph inside the hero area

--- capstone-project/src/pages/Home.jsx
+++ capstone-project/src/pages/Home.jsx
@@ -2,8 +2,12 @@ import '../index.css';
 import { HeroArea } from '../components/HeroArea';
 
 function Home() {
-    return(
-    <HeroArea />)
+  return (
+    <HeroArea
+      heading="Where healing and community is built"
+      paragraph="Join a community dedicated to lifting one another up. Share your prayer request and experience the strength of being known and supported"
+    />
+  );
 }
 
 export { Home };
