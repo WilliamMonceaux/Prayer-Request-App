b8e53f335b9a147e6e3bff74c28a145461f1a17f
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-02-21 10:16:16 -0600

    imported components for home page and mui material

--- prayer-request-website/src/app/page.js
+++ prayer-request-website/src/app/page.js
@@ -1,8 +1,29 @@
-
+'use client';
+import { HeroArea } from '@/components/HeroArea';
+import { PrayerRequestCards } from '@/components/PrayerRequestCards';
+import { UnderstandingPrayer } from '@/components/UnderstandingPrayer';
+import { Typography, Container } from '@mui/material';
 
 export default function Home() {
   return (
-  <main>
-    <h1>Prayer Request Project</h1>
-  </main>);
+    <>
+      <header>
+        <HeroArea
+          heading="Where healing and community is built"
+          paragraph="Join a community dedicated to lifting one another up. Share your prayer request and experience the strength of being known and supported"
+          button="Create an Account"
+        />
+        <UnderstandingPrayer />
+      </header>
+
+      <main>
+        <Container maxWidth="md" sx={{ mt: 4 }}>
+          <Typography variant="h4" component="h2" gutterBottom sx={{ textAlign: 'center', fontWeight: 'bold' }}>
+            Community Prayer Requests
+          </Typography>
+        </Container>
+        <PrayerRequestCards />
+      </main>
+    </>
+  );
 }
