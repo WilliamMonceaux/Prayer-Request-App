fa914a8412903003244cfdbd5eb0a09b2d8b5a43
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-02-26 07:41:19 -0600

    moved the successfully connected to Mongo console.log into the promise; refactored with prettier

--- prayer-request-website/src/lib/mongodb.js
+++ prayer-request-website/src/lib/mongodb.js
@@ -3,34 +3,34 @@ import mongoose from 'mongoose';
 const MONGO_URI = process.env.MONGO_URI;
 
 if (!MONGO_URI) {
-    throw new Error('Please give a valid environment uri variable');
+  throw new Error('Please give a valid environment uri variable');
 }
 
 let cached = global.mongoose;
 
 if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null};
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
 async function connectMongo() {
-    if (cached.conn) return cached.conn;
+  if (cached.conn) return cached.conn;
 
-    if (!cached.promise) {
-        console.log('Starting new MongoDB connection...')
-        cached.promise = mongoose.connect(MONGO_URI)
-        .then((m) => m);
+  if (!cached.promise) {
+    console.log('Starting new MongoDB connection...');
+    cached.promise = mongoose.connect(MONGO_URI).then((m) => {
+      console.log('Successfully connected to MongoDB');
+      return m;
+    });
+  }
 
-        try {
-            cached.conn = await cached.promise;
-            console.log('Successfully connected to MongoDB');
-        } catch (err) {
-            cached.conn = null;
-            console.error('Failed to connect to MongoDB');
-            throw err
-        }
-        return cached.conn;
-    }
-    return mongoose.connect(MONGO_URI);
+  try {
+    cached.conn = await cached.promise;
+  } catch (err) {
+    cached.conn = null;
+    console.error('Failed to connect to MongoDB');
+    throw err;
+  }
+  return cached.conn;
 }
 
 export { connectMongo };
