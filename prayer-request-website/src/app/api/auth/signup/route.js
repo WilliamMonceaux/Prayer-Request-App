b36a270fa771535b5d70c7116d8e152ba14b4e4f
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-03-07 17:27:55 -0600

    imported path and fs for profile pictures verification; implemented profile picture to appear on profile picture on navbar when a user uploads a photo

--- prayer-request-website/src/app/api/auth/signup/route.js
+++ prayer-request-website/src/app/api/auth/signup/route.js
@@ -1,12 +1,18 @@
 import { NextResponse } from 'next/server';
 import { connectMongo } from '@/lib/mongodb';
+import path from 'path';
+import fs from 'fs/promises';
 import { User } from '@/models/User';
 
 export async function POST(request) {
   try {
     await connectMongo();
 
-    const { username, email, password } = await request.json();
+    const data = await request.formData();
+    const username = data.get('username');
+    const email = data.get('email');
+    const password = data.get('password');
+    const file = data.get('profilePicture');
 
     if (!username || !email || !password) {
       return NextResponse.json({ message: 'Missing fields' }, { status: 400 });
@@ -18,14 +24,39 @@ export async function POST(request) {
       return NextResponse.json({ message: 'User already exists' }, { status: 400 });
     }
 
+    let profilePicturePath = '';
+
+    if (file && typeof file !== 'string') {
+      const bytes = await file.arrayBuffer();
+      const buffer = Buffer.from(bytes);
+
+      const filename = `${Date.now()}-${file.name}`;
+      const uploadDir = path.join(process.cwd(), 'public', 'uploads');
+      const savePath = path.join(uploadDir, filename);
+
+      await fs.mkdir(uploadDir, { recursive: true });
+
+      await fs.writeFile(savePath, buffer);
+      profilePicturePath = `/uploads/${filename}`;
+    }
+
     const newUser = await User.create({
       username,
       email,
       password,
+      profilePicture: profilePicturePath,
     });
 
     return NextResponse.json(
-      { message: 'User registered!', userId: newUser._id },
+      {
+        message: 'User registered!',
+        user: {
+          _id: newUser._id,
+          username: newUser.username,
+          email: newUser.email,
+          profilePicture: newUser.profilePicture,
+        },
+      },
       { status: 201 }
     );
   } catch (error) {
